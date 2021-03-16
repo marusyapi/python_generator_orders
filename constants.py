e977fe584a638935d52b43df1e51acb810cbba80
marusya_pi <mariia.pitiukova@gmail.com>
2021-03-16 13:02:24 +0200

    write csv functions, logger, code clean up

--- constants.py
+++ constants.py
@@ -1,17 +1,37 @@
+import logging
+from pathlib import Path
 import yaml
 
 
-with open(r'config.yaml') as configfile:
-    config = yaml.load(configfile, Loader=yaml.FullLoader)
+def open_config_file():
+    try:
+        with open(r'config.yaml') as configfile:
+            config_file = yaml.load(configfile, Loader=yaml.FullLoader)
+        return config_file
+    except FileNotFoundError:
+        logging.warning("The config file's missing")
+        return Path.home() / 'config.yaml'
 
+
+config = open_config_file()
+
+CONFIG_PATH = 'config.yaml'
+LOGGER_MESSAGE = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOGGER_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+LOGGER_PATH = 'logger.txt'
+CSV_PATH = 'orders.csv'
+SQL_CSV_PATH = 'sql.csv'
 ID_INITIAL = int(config['id_initial'], 16)
 ORDERS_COUNT = config['Orders']['count_orders']
+ORDER_ATTRIBUTES = ["ID", "SIDE", "INSTRUMENT", "PX_INIT", "VOLUME_INIT", "DATE", "STATUS", "NOTE", "TAGS"]
+SQL_ATTRIBUTES = ["QUERY"]
+SQL_ORDER_ATTRIBUTES = 'ID', 'SIDE', 'INSTRUMENT', 'PX_INIT', 'VOLUME_INIT', 'DATE', 'STATUS', 'NOTE', 'TAGS'
+FORMAT_DISPLAYING_ORDERS = '{:<13}{:<7}{:<13}{:<14}{:<15}{:<30}{:<17}{:<14}{:<13}'
 RED_ZONE_ORDERS_COUNT = config['Orders']['ordersRed']
 GREEN_ZONE_ORDERS_COUNT = config['Orders']['ordersGreen']
 BLUE_ZONE_ORDERS_COUNT = config['Orders']['ordersBlue']
 LIST_OF_INSTRUMENTS = list(config['Instrument']['List'].items())
 INSTRUMENTS_COUNT = 11
-NULL_RANGE = 0
 DECIMAL_NUMBER_ROUND = 1
 DIVIDER_PX_INIT = 6075
 DIVIDER_PX_FILL = 7875
@@ -33,3 +53,5 @@ LIST_OF_NOTES = config['Note']
 NOTES_COUNT = len(config['Note'])
 LIST_OF_TAGS = config['Tag']
 TAGS_COUNT = len(config['Tag'])
+LEFT = 0
+RIGHT = 1
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from generator import *
+from pypika import Query, Table
+import pandas
+from constants import *
+from orders import build_orders
+
+
+def logger():
+    path = create_file_path(LOGGER_PATH)
+
+    logging.basicConfig(filename=path, filemode='w', level=logging.DEBUG, format=LOGGER_MESSAGE,
+                        datefmt=LOGGER_DATE_FORMAT)
+    log = logging.getLogger(__name__)
+    return log
+
+
+def create_file_path(path):
+    try:
+        file_path = Path(path)
+    except OSError:
+        logger.warning("Failed to create file.")
+        return LEFT
+    return file_path
+
+
+logger = logger()
 
 
 def init():
@@ -13,8 +37,64 @@ def workflow():
     pass
 
 
+def generate_sql_list():
+    orders = Table('Orders')
+
+    orders_list = build_orders()
+    orders_sql_list = []
+
+    for i in orders_list:
+        orders_sql_list.append((Query.from_(orders).into(SQL_ORDER_ATTRIBUTES).insert(i)).get_sql())
+
+    return orders_sql_list
+
+
 def out():
-    pass
+    logger.info("Generating a list of orders")
+    orders = build_orders()
+    logger.info(f"The list's been generated successfully.")
+
+    try:
+        csv_path = create_file_path(CSV_PATH)
+        sql_csv_path = create_file_path(SQL_CSV_PATH)
+    except KeyError:
+        logger.warning("Missing CSV's path file. Creating a file.")
+        csv_path = CSV_PATH
+        sql_csv_path = SQL_CSV_PATH
+
+    logger.info(f"Writing a list to a csv file. \nPath: {csv_path}.")
+    write_csv_file(csv_path, orders, ORDER_ATTRIBUTES)
+
+    logger.info(f"Reading a csv file. \nPath: {csv_path}.")
+    read_csv_file(csv_path)
+
+    sql_orders = generate_sql_list()
+    logger.info(f"The list's been generated successfully.")
+
+    logger.info(f"Writing a queries to a csv file. \nPath: {sql_csv_path}.")
+    write_csv_file(sql_csv_path, sql_orders, SQL_ATTRIBUTES)
+
+    logger.info(f"Reading a sql csv file. \nPath: {sql_csv_path}.")
+    read_csv_file(sql_csv_path)
+
+
+def write_csv_file(path, list, columns):
+    try:
+        csv_file = pandas.DataFrame(data=list, columns=columns)
+        csv_file.to_csv(path, index_label=False, sep='\t')
+        return csv_file
+    except OSError:
+        logger.error(f"Failed to write data.")
+        return LEFT
+
+
+def read_csv_file(file):
+    try:
+        csv_file = pandas.read_csv(file, sep='\t')
+        return csv_file
+    except OSError:
+        logger.error(f"Failed to read data.")
+        return LEFT
 
 
 if __name__ == '__main__':
@@ -23,5 +103,5 @@ if __name__ == '__main__':
         setup()
         workflow()
         out()
-    except Exception as e:
-        logger.error(e)
+    except Exception as ex:
+        logging.error(ex)
